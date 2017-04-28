76584f711b89ebd3402f3924e57615db64378cfc
Albert Zak <me@albertzak.com>
2017-04-28 21:29:22 +0200

    Add more verbose logging

--- src/broadcasting.js
+++ src/broadcasting.js
@@ -13,7 +13,7 @@ const createSocket = (callback) => {
 }
 
 const broadcast = (socket, message) => {
-  console.log('Broadcasting', typeof message, message)
+  console.log('Broadcasting', typeof message, message, 'as string: ' + message)
   socket.send(message, broadcastPort, broadcastAddress, (err) => {
     if (err) {
       console.error(err)
