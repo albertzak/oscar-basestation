ee4129a036e616172691d0e07dfb5b0155502523
Karim <k.schierbauer@gmx.at>
2017-05-19 16:24:26 +0200

    test
    broadcast 0-200

--- src/main.js
+++ src/main.js
@@ -18,9 +18,13 @@ const main = async () => {
     buffer.on('chunk', (chunk) => {
       socket.broadcast(chunk)
     })
-
+var i = 0;
     setInterval(() => {
-      socket.broadcast('ping')
+      
+      socket.broadcast(i)
+      if (i ==200) {
+        i = 0;
+      }
     }, 1000)
   } catch (e) {
     console.log('xxx', e)
