0fc97eaa65dcf4720befd18cfe98fdcfbc18f0fc
m4xwe11o <maximilian.pessl@gmail.com>
2017-05-19 21:14:35 +0200

    Revert "remove argument"
    This reverts commit f6896a3532f1647de75777cf05554670f2d67932.
    
    # Conflicts:
    #	src/serial.js

---
+++ src/serial.js
@@ -0,0 +1,25 @@
+const Serialport = require('serialport')
+
+const portName = process.env.XBEE_SERIAL_PORT || '/dev/ttyS0'  // This is the standard Raspberry Pi Serial port
+
+const serial = (onData) => {
+  const sp = new Serialport(portName, {
+    baudRate: 9600,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+  })
+
+  console.log('Opening seriaport', portName)
+
+  sp.on('open', () => {
+    console.log('Opened seriaport', portName)
+
+    sp.on('data', (data) => {
+      console.log('Serial data received', data)
+      onData(null, data)
+    })
+  })
+}
+
+module.exports = { serial }
